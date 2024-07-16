42fe3827deaf7d209945d7472e95f89e95b0f532
ES-SAADANI Mohamed <mohamed.essaadani@adria-bt.com>
2024-07-16 10:03:30 +0100

    api tests

--- src/test/java/org/essaadani/springboottesting/controller/PlayerRESTAPIUnitTest.java
+++ src/test/java/org/essaadani/springboottesting/controller/PlayerRESTAPIUnitTest.java
@@ -1,18 +1,14 @@
 package org.essaadani.springboottesting.controller;
 
 import org.essaadani.springboottesting.service.PlayerService;
-import org.essaadani.springboottesting.service.PlayerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -23,9 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class PlayerRESTAPIUnitTest {
     @MockBean
     PlayerService playerService;
-
-    @InjectMocks
-    PlayerRESTAPI playerRESTAPI;
 
     @Autowired
     MockMvc mockMvc;
@@ -47,6 +40,11 @@ class PlayerRESTAPIUnitTest {
     }
 
     @Test
-    void getAll() {
+    void getAll() throws Exception {
+        mockMvc.perform(
+                        get("/api/players")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
     }
 }
